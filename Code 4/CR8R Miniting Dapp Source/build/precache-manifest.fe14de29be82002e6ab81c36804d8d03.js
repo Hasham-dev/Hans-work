5944c23a8afab61ba0bee54ba09f0d495b13daa8
Hasham-dev <vakanihasham64@gmail.com>
2024-06-25 07:46:06 +0500

    build changes

---
+++ Code 4/CR8R Miniting Dapp Source/build/precache-manifest.fe14de29be82002e6ab81c36804d8d03.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038fe33e4a27c008deabce10405dd53a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47af3578e7aa8ea7eb6f",
+    "url": "/static/css/main.080b74a2.chunk.css"
+  },
+  {
+    "revision": "c0b9072661cd5d0ed2da",
+    "url": "/static/js/2.a4281270.chunk.js"
+  },
+  {
+    "revision": "1be320caa356c1af9c7b3ba38734b654",
+    "url": "/static/js/2.a4281270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47af3578e7aa8ea7eb6f",
+    "url": "/static/js/main.120c1204.chunk.js"
+  },
+  {
+    "revision": "a98e714b7b6d52c2150b",
+    "url": "/static/js/runtime-main.2fe33eac.js"
+  },
+  {
+    "revision": "16729765e4a707193bd96386f84960fa",
+    "url": "/static/media/Floating-NiFTis.16729765.gif"
+  },
+  {
+    "revision": "6a9ea3a19f64d9a5556d1ddec2f15504",
+    "url": "/static/media/background.6a9ea3a1.mp4"
+  },
+  {
+    "revision": "b40da26c7656ef5c783ac522b702b0a1",
+    "url": "/static/media/logo.b40da26c.png"
+  }
+]);
