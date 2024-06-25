76f97bcdcb6f0c8eb1bbf3ba5431e0380e89b908
Hasham-dev <vakanihasham64@gmail.com>
2024-06-25 07:44:47 +0500

    add code 4

--- Code 4/CR8R Miniting Dapp Source/src/App.js
+++ Code 4/CR8R Miniting Dapp Source/src/App.js
@@ -1,8 +1,10 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/header/header";
 import Hero from "./components/hero/Hero";
 import Mint from "./components/mint/mint";
 import Video from "./components/video/video";
+import { CustomFooter } from "./components/header/header";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Header />
       <Hero />
       <Mint />
+      <CustomFooter />
     </div>
   );
 }
